1a7bb9cfa781ddfda5f5c7de72b916a3d73c6527
romety2 <lmielewczyk@sigma.ug.edu.pl>
2015-11-01 20:50:31 +0100

    Sprawdzenie polaczenia z baza danych

--- jdbc/src/main/java/com/mielewczykl/jdbc/model/Klasztor.java
+++ jdbc/src/main/java/com/mielewczykl/jdbc/model/Klasztor.java
@@ -3,21 +3,28 @@ package com.mielewczykl.jdbc.model;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Klasztor {
-
-	public static void main(String[] args) {
-		try
-		{
+	Klasztor() {
+		try {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 			Connection db = DriverManager.getConnection("jdbc:sqlserver://eos.inf.ug.edu.pl;databaseName=lmielewczyk;trustServerCertificate=true", "lmielewczyk", "224701");
+			Statement st = db.createStatement();
+			st.execute("INSERT INTO religia(religia, opis) VALUES ('string', 'string');");
+			st.execute("INSERT INTO klasztor(id_religia, nazwa, kontakt) VALUES (1, 'string', 'string');");
+			st.close();
 			db.close();
-		}
+			}
 		catch (SQLException sqle)
 		{
 		}
 		catch (ClassNotFoundException cnfe)
 		{
 		}
+	}
+	public static void main(String[] args)
+	{
+		new Klasztor();
 	}
 }
